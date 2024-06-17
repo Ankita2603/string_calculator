44b402e721c97372e8803d74cfc203fd14dd4df9
ankita <nandanwarankita26@gmail.com>
2024-06-17 14:04:28 +0530

    initial commit - Add test for empty string

--- spec/string_calculation_spec.rb
+++ spec/string_calculation_spec.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require_relative '../string_calculation'
 
 RSpec.describe StringCalculator do
-	describe '#add' do
+  describe '#add' do
     it 'returns 0 for an empty string' do
-      string_addition = StringAddition.new()
-      expect(string_addition.add("")).to eq(0)
+      string_addition = StringCalculator.new("")
+      expect(string_addition.add).to eq(0)
     end
-	end
+  end
 end
--- string_calculation.rb
+++ string_calculation.rb
@@ -1,16 +1,9 @@
 class StringCalculator
-	def initialize(string_numbers)
-		@string_numbers = string_numbers
-	end
+  def initialize(string_numbers)
+    @string_numbers = string_numbers
+  end
 
-	def add
-		char_ar = string_numbers.chars
-		sum = 0
-		char_ar.each do |chr|
-			int = chr.to_i
-			next if int == 0
-			sum += int
-		end
-		sum
-	end
+  def add
+    0
+  end
 end
